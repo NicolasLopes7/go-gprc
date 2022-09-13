0a72bf9d9e5594832f2f2d57e7a465ad7c01de6b
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-09-13 13:02:14 +0100

    feat(user): create user service and register on the server

---
+++ services/user.go
@@ -0,0 +1,28 @@
+package services
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/NicolasLopes7/gprc-go/pb"
+)
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func NewUserService() *UserService {
+	return &UserService{}
+}
+
+func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error) {
+
+	// Insert - Database
+	fmt.Println(req.Name)
+
+	return &pb.User{
+		Id:    "123",
+		Name:  req.GetName(),
+		Email: req.GetEmail(),
+	}, nil
+}
